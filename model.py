86e55c35a8f09647f820fa3f14d505bcc887c56d
Екатеринина Дарья <dashae040804@gmail.com>
2025-11-05 22:42:58 +0500

    Praktika 4

---
+++ model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class Todo(BaseModel):
+    id: int
+    item: str
+
+    class Config:
+        Schema_extra = {
+            "Example": {
+                "id": 1,
+                "item": "Example schema!"
+            }
+        } 
